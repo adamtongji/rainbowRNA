ed95904d4f9ec18cbeba9c02ba223fabca68fdef
adamtongji <adam.tongji@gmail.com>
2017-12-10 14:11:47 +0800

    12.10

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #git clone https://github.com/adamtongji/rainbowRNA
-conda create -c R -c bioconda -c default --file requirement.txt -n rainbow_env
+conda create -n rainbow_env python=2.7 -c R -c bioconda -c default --file requirement.txt
 source activate rainbow_env
 mkdir -p ~/bin
 myPath=`pwd`
 
-ln -s $myPath/__main__.py ~/bin/rainbowRNA
+ln -s $myPath/__main__.py > ~/bin/rainbowRNA
 echo $myPath > ~/bin/.rainbow_path_store
 chmod 500 ~/bin/.rainbow_path_store
