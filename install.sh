494d8af45279f840737876718dcfa1288422abf3
adamtongji <adam.tongji@gmail.com>
2017-12-10 14:05:26 +0800

    12.10

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #git clone https://github.com/adamtongji/rainbowRNA
-conda install -c R -c bioconda -c default --file requirement.txt -n rainbow_env
+conda create -c R -c bioconda -c default --file requirement.txt -n rainbow_env
 source activate rainbow_env
 mkdir -p ~/bin
 myPath=`pwd`
---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+conda-env remove rainbow_env
+echo "Remove installed environment!"
+rm ~/bin/.rainbow_path_store
+rm ~/bin/rainbowRNA
+
+echo "Uninstall completed!!"
