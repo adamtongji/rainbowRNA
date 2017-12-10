118f0bee506905e834adac7a15778b45512d21f0
adamtongji <adam.tongji@gmail.com>
2017-12-10 13:42:18 +0800

    12.10

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@
 conda install -c R -c bioconda -c default --file requirement.txt -n rainbow_env
 source activate rainbow_env
 mkdir -p ~/bin
-ln -s rainbowRNA/__main__.py ~/bin/
+myPath=`pwd`
+ln -s $myPath/__main__.py ~/bin/
 
